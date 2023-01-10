ceca5a099a3a966024d1aa63a53b8908e0dcdc42
y.el-haddady <youssef.el-haddady@nimbelways.com>
2023-01-10 22:24:58 +0100

    feat: create basic example with CRA

---
+++ examples/with-cra/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {
+  Feature,
+  FlagsProvider,
+  useFeature,
+  withFeature,
+} from '@featflag/react';
+import flags from './flags.json';
+
+function App() {
+  return (
+    <FlagsProvider features={flags}>
+      <div className="App">
+        <Feature flag="feat">this should be in DOM</Feature>
+        <Feature flag="featDisabled">this should be hidden</Feature>
+        <Feature
+          flag="featDisabled"
+          renderOff={<p>This feature is disabled</p>}
+        >
+          Enjoy your amazing feat
+        </Feature>
+        <WithUseFeatureHook />
+        <TestWithFeatureHOC />
+      </div>
+    </FlagsProvider>
+  );
+}
+
+function WithUseFeatureHook() {
+  const isNewUiActive = useFeature('hookSupport');
+
+  return (
+    <p style={{ color: isNewUiActive ? 'green' : 'red' }}>Test with hooks</p>
+  );
+}
+
+function WithFeatureHOC() {
+  return <p style={{ color: 'green' }}>Test with withFeature (HOC)</p>;
+}
+
+const TestWithFeatureHOC = withFeature('hocSupport')(WithFeatureHOC);
+
+export default App;
